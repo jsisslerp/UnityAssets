c94c62d0b708c9d59e950b84a1e55f7462f4ee9c
John Sissler <jsissler@gmail.com>
2019-05-26 19:43:23 -0400

    Added lock

--- Util/Assets/Scripts/PersistentGameObjectSingleton.cs
+++ Util/Assets/Scripts/PersistentGameObjectSingleton.cs
@@ -3,22 +3,26 @@ using UnityEngine;
 
 public class PersistentGameObjectSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static PersistentGameObjectSingleton<T> Instance { get; private set; }
+    private readonly object instanceLock = new object();
+    public static T Instance { get; private set; }
 
     void Awake()
     {
-        // If no Instance yet exists, assign and preserve across scenes.
-        if (!Instance)
+        lock (instanceLock)
         {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-            Debug.Log("created singleton instance of type " + name);
-        }
-        // Otherwise, if an Instance already exists, destroy the new instance.
-        else if (Instance != this)
-        {
-            Debug.Log("destroying new instance of " + name + " singleton");
-            DestroyImmediate(gameObject);
+            // If no Instance yet exists, assign and preserve across scenes.
+            if (!Instance)
+            {
+                Instance = (T)(object)this;
+                DontDestroyOnLoad(gameObject);
+                Debug.Log("created singleton instance of type " + name);
+            }
+            // Otherwise, if an Instance already exists, destroy the new instance.
+            else if (Instance != this)
+            {
+                Debug.Log("destroying new instance of " + name + " singleton");
+                DestroyImmediate(gameObject);
+            }
         }
     }
 }
--- Util/Assets/Sprite.cs
+++ Util/Assets/Sprite.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sprite : MonoBehaviour {
+public class Sprite : PersistentGameObjectSingleton<Sprite>
+{
 
 	// Use this for initialization
 	void Start () {
